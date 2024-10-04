e58e1080d81659365909082b079ed3c72dee97f4
harold2001 <hcarazasnires@gmail.com>
2024-10-03 21:58:24 -0500

    product url fixed

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,4 +1,4 @@
-const baseURL = import.meta.env.VITE_SERVER_URL
+const baseURL = import.meta.env.VITE_SERVER_URL;
 
 function convertToJson(res) {
   if (res.ok) {
@@ -10,12 +10,12 @@ function convertToJson(res) {
 
 export default class ProductData {
   async getData(category) {
-    const response = await fetch(baseURL + `products/search/${category}`);
+    const response = await fetch(baseURL + `/products/search/${category}`);
     const data = await convertToJson(response);
     return data.Result;
   }
   async findProductById(id) {
-    const response = await fetch(baseURL + `product/${id}`);
+    const response = await fetch(baseURL + `/product/${id}`);
     const data = await convertToJson(response);
     return data.Result;
   }
