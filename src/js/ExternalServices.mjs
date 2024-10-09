136144ccd21567b41299c0133a870f90a2297ddd
harold2001 <hcarazasnires@gmail.com>
2024-10-09 11:39:21 -0500

    CheckoutProcess.mjs finished

--- src/js/ExternalServices.mjs
+++ src/js/ExternalServices.mjs
@@ -1,5 +1,4 @@
 const baseURL = import.meta.env.VITE_SERVER_URL;
-
 function convertToJson(res) {
   if (res.ok) {
     return res.json();
@@ -8,7 +7,7 @@ function convertToJson(res) {
   }
 }
 
-export default class ProductData {
+export default class ExternalServices {
   async getData(category) {
     const response = await fetch(baseURL + `products/search/${category}`);
     const data = await convertToJson(response);
@@ -18,5 +17,15 @@ export default class ProductData {
     const response = await fetch(baseURL + `product/${id}`);
     const data = await convertToJson(response);
     return data.Result;
+  }
+  async checkout(payload) {
+    const options = {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(payload),
+    };
+    return await fetch(baseURL + "checkout/", options).then(convertToJson);
   }
 }
